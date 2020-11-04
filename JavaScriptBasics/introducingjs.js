09d95bf6c2be3214502f84507968c009d99b9ad5
dennis00095 <mwongeladennis9@gmail.com>
2020-11-04 15:13:21 +0300

    wrote JavsScript first program

---
+++ JavaScriptBasics/introducingjs.js
@@ -0,0 +1,28 @@
+alert("Hello welcome JavaScript"); //statement
+document.write("<h1>Lets learn JavaScript basics together</h1>");
+alert("Goodbye, come bak again and learn some more!");
+
+
+/*
+
+>> alert(); is a command that opens a dialog box on the browser
+document.write();
+
+    >document.write() is used to write directly to the webpage.
+
+    > document represents the current webkitConvertPointFromPageToNode, while the write
+    represents a command that write the message to the page!
+
+>> Every javascript statement needs to end with a semicolon just like a sentence would end
+with a fullstop/period.
+
+>> The above javascript program runs one statement after the other, as a program is supposed
+to run.
+
+>> A statement needs to be complete before the next one can run.
+
+
+    
+    
+    
+    */
