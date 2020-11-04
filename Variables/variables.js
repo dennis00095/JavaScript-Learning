1e401c7d45a0a1d752adbde56fd9baf146c42212
dennis00095 <mwongeladennis9@gmail.com>
2020-11-04 16:11:32 +0300

    Variable introduction

---
+++ Variables/variables.js
@@ -0,0 +1,14 @@
+/*Variables are used to store and track information in a PerformanceNavigationTiming.
+        the basic syntax for creating a variable is as follows:
+
+            var nameOfYourVariable = 0;
+
+*/
+
+var greeting = "Hello!";
+alert(greeting);
+
+// you can change the content of a variable to print a different value assigned to them.
+
+greeting = "Hello dear esteemed visitor";
+alert(greeting);
