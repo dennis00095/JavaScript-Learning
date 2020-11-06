cdbb965d11324b4c4492989f99e29840c1b551f3
dennis00095 <mwongeladennis9@gmail.com>
2020-11-06 20:28:03 +0300

    Most common value types

---
+++ ValueTypes/valuetypes.js
@@ -0,0 +1,15 @@
+/* The most common value types that you're likely to encounter are Strings and Numbers
+Strings are like sentences, JavaScript understands strings by using quote marks
+Double quotes ("") or single ('') can be used to define a string, but you have to make sure
+that you're consistent. That is if you use single quotes to open a string, do not end the string
+with a double quote, JavaScript will not be able to understand.
+
+You can use both double and single quote in one string; 
+    for example
+*/
+        alert("It's nice to meet you");
+                    //or
+        var htmlsnippet = '<h2 class="special">A very important headline</h2>';
+        alert(htmlsnippet);
+
+       
