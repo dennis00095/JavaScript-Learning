cf802ac1a767ab1accc5ef3eb3e8f8693b421b30
dennis00095 <mwongeladennis9@gmail.com>
2020-11-06 20:57:22 +0300

    Adding of strings together

---
+++ ValueTypes/concantenation.js
@@ -0,0 +1,6 @@
+/*JavaScript can be used to collect information from users of a BhxBrowser, the information
+collected can be combined to print afinal fine result*/
+
+var newVisitor = prompt('What is your name?');
+var message = 'Hi ' + newVisitor;
+document.write(message);
